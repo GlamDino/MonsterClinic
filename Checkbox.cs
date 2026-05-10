bfb17932efcb57485ddc7b7f8483c885382c0344
Astra Fox <fox.nieto44@gmail.com>
2026-05-10 19:07:59 +0200

    Prototype
    Computer intereface, prototype, office prototype, patient interface prototype (What we submited on 1st milestone)

---
+++ Checkbox.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class Checkbox : Button
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+        Pressed += ButtonPressed;
+    }
+    private void ButtonPressed()
+    {
+        Text = "x";
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+	{
+	}
+}
